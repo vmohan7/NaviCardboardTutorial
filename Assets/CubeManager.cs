ddd9f3080ccf1a131302fc7f3dacef324f11719d
Vasanth Mohan <vmohan7@stanford.edu>
2015-09-28 11:08:39 -0700

    Inital Commit of tutorial

---
+++ Assets/CubeManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(ParticleSystem))]
+public class CubeManager : MonoBehaviour {
+
+	private ParticleSystem ps;
+	private Vector2 prevPos;
+
+	void Start () {
+		ps = GetComponent<ParticleSystem> ();
+	}
+
+	void OnDestroy(){
+		//Called when GameObject is destroyed
+	}
+
+	private void ChangeToRandomColor(){
+		ps.startColor = new Color (Random.Range (0F, 1F), Random.Range (0F, 1F), Random.Range (0F, 1F));
+	}
+
+}
